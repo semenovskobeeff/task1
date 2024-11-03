5a375eaea1546f928ff4c076536ca3c6c22d8cb3
stanislav <sssemenov@rt-dc.ru>
2024-11-04 00:07:04 +0300

    Первая часть задания

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,20 @@
 import logo from "./logo.svg";
-import { createElement } from "react";
 import "./App.css";
 
 export const App = () => {
-	return createElement(
-		"div",
-		{ className: "App" },
-		createElement(
-			"header",
-			{ className: "App-header" },
-			createElement("img", { src: logo, className: "App-logo", alt: "logo" }),
-			createElement("p", null, "Edit ", createElement("code", null, "src/App.js"), " and save to reload 2."),
-			createElement(
-				"a",
-				{ className: "App-link", href: "https://reactjs.org", target: "_blank", rel: "noopener noreferrer" },
-				"Learn React",
-			),
-			createElement("p", null, new Date().getFullYear()),
-		),
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload 2.
+				</p>
+				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+					Learn React
+				</a>
+				<p>{new Date().getFullYear()}</p>
+			</header>
+		</div>
 	);
 };
-// createElement - императивный стиль
+// Используется только декларатиный стиль
